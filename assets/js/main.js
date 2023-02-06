c854180a9e696ac92afbb82f0a41cb79f6f24afa
unknown <mahmoudnagah999@gmail.com>
2023-02-06 14:07:39 +0200

    first_push

---
+++ assets/js/main.js
@@ -0,0 +1,93 @@
+// CHANGE BACKGROUND HEADER
+const scrollHeader = ()=>{
+    const header = document.getElementById('header')
+    //when the scroll is greater than 50 viewport height, and the scroll-header class to header tag
+    this.scrollY >=50 ? header.classList.add('scroll-header')
+                      : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+// Swiper Products
+let swiperProducts = new Swiper(".products__container", {
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPreView: 'auto',
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+        1024: {
+            spaceBetween: 72,
+        },
+    },
+});
+
+// Scroll Sections Active Link
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () =>{
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 58,
+        sectionId = current.getAttribute('id'),
+        sectionsClasse = document.querySelector('.nav__menu a[href*=' + sectionId +']')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            sectionsClasse.classList.add('active-link')
+        } else {
+            sectionsClasse.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+// show Scroll up
+const themeButton = document.getElementById('theme-button') 
+ const darkTheme = 'dark-theme' 
+ const iconTheme = 'ri-sun-line' 
+  
+ // Previously selected topic (if user selected) 
+ const selectedTheme = localStorage.getItem('selected-theme') 
+ const selectedIcon = localStorage.getItem('selected-icon') 
+  
+ // We obtain the current theme that the interface has by validating the dark-theme class 
+ const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light' 
+ const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line' 
+  
+ // We validate if the user previously chose a topic 
+ if (selectedTheme) { 
+   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark 
+   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme) 
+   themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme) 
+ } 
+  
+ // Activate / deactivate the theme manually with the button 
+ themeButton.addEventListener('click', () => { 
+    // Add or remove the dark / icon theme 
+    document.body.classList.toggle(darkTheme) 
+    themeButton.classList.toggle(iconTheme) 
+    // We save the theme and the current icon that the user chose 
+    localStorage.setItem('selected-theme', getCurrentTheme()) 
+    localStorage.setItem('selected-icon', getCurrentIcon()) 
+ })
+
+// scroll Reveal Animation
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+})
+
+sr.reveal(`.home__data, .products__container, .footer__container, .footer__info`)
+sr.reveal(`.home__images`, {delay: 600, origin: 'bottom'})
+sr.reveal(`.new__card, .brand__img`, {interval: 100})
+sr.reveal(`.collection__explore:nth-child(1)`, {origin: 'right'})
+sr.reveal(`.collection__explore:nth-child(2)`, {origin: 'left'})
